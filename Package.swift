693e6002678f97cff1ac6a598750be307273985d
Cocoa <i@uwucocoa.moe>
2026-08-13 19:04:22 +0000

    release: prepare v0.1.0

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@
 
 import PackageDescription
 
+let bondryVersion = "0.1.0"
+let releaseBaseURL =
+  "https://github.com/bondry-dev/bondry/releases/download/v\(bondryVersion)"
+
 let package = Package(
   name: "Bondry",
   platforms: [
@@ -15,15 +19,15 @@ let package = Package(
     .library(name: "BondryLocalServer", targets: ["BondryLocalServer"]),
   ],
   targets: [
-    .target(
+    .binaryTarget(
       name: "CBondryRuntime",
-      path: "apple/Sources/CBondryRuntime",
-      publicHeadersPath: "include"
+      url: "\(releaseBaseURL)/BondryRuntime.xcframework.zip",
+      checksum: "f276fcba7bf7db076f84809f9433567db1886a5d96d5080b83bb9394ddf6f4c9"
     ),
-    .target(
+    .binaryTarget(
       name: "CBondryLocalServer",
-      path: "apple/Sources/CBondryLocalServer",
-      publicHeadersPath: "include"
+      url: "\(releaseBaseURL)/BondryLocalServer.xcframework.zip",
+      checksum: "79458fd84b624d65ccad7b1c9d3019f6c38701b56dd57f99f46e6a42102044c2"
     ),
     .target(
       name: "BondryApple",
