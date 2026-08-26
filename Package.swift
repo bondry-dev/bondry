c0e72f7962da3eb1b1f59a136e3bc8ec91a212f1
Cocoa <i@uwucocoa.moe>
2026-08-26 11:35:59 +0000

    release: prepare v0.2.1

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let bondryVersion = "0.2.0"
+let bondryVersion = "0.2.1"
 let releaseBaseURL =
   "https://github.com/bondry-dev/bondry/releases/download/v\(bondryVersion)"
 
@@ -17,6 +17,7 @@ let package = Package(
     .library(name: "BondryApple", targets: ["BondryApple"]),
     .library(name: "BondryAppIntents", targets: ["BondryAppIntents"]),
     .library(name: "BondryLocalServer", targets: ["BondryLocalServer"]),
+    .library(name: "BondryRESTServer", targets: ["BondryRESTServer"]),
     .library(name: "BondryEgress", targets: ["BondryEgress"]),
     .library(name: "BondryWebhookIngress", targets: ["BondryWebhookIngress"]),
   ],
@@ -24,22 +25,27 @@ let package = Package(
     .binaryTarget(
       name: "CBondryRuntime",
       url: "\(releaseBaseURL)/BondryRuntime.xcframework.zip",
-      checksum: "9d848465892c2d00d3b6f1f38034cc90b966361bfb0b1cbe1e36b567af65dbf4"
+      checksum: "273f8949283a7f1b4288b8b74280e873c5440fc17b09aa47995f47319f1e21ab"
     ),
     .binaryTarget(
       name: "CBondryLocalServer",
       url: "\(releaseBaseURL)/BondryLocalServer.xcframework.zip",
-      checksum: "ff73300b28195ec542f403e7ed96c349e3aaca06fac1fd8158b6db60b077875e"
+      checksum: "a4e72ff50b095bd38716cb49abb6823d23cc418328f2c3ed82bb5becb62b4643"
+    ),
+    .binaryTarget(
+      name: "CBondryRESTServer",
+      url: "\(releaseBaseURL)/BondryRESTServer.xcframework.zip",
+      checksum: "b77cd6e3617644da88590a0723dd12c58bb8ebb0fe064df49a0a688158e90303"
     ),
     .binaryTarget(
       name: "CBondryEgress",
       url: "\(releaseBaseURL)/BondryEgress.xcframework.zip",
-      checksum: "3e2a0a47724971d573c07e8d5279a6b4633871c8164924f762e7e023a6a75328"
+      checksum: "1ec1aff2b241671344619752a72430955c26bfba630ce8c44f97362a3149e69c"
     ),
     .binaryTarget(
       name: "CBondryWebhookIngress",
       url: "\(releaseBaseURL)/BondryWebhookIngress.xcframework.zip",
-      checksum: "c940be1ff5b7247d0f8a6960bd8fb06d2f7c38e25432062d03da06961335ce6b"
+      checksum: "3b95e7193e7ca8d68386445b6602ffff9050050bf103bd33ead09c93875f2c73"
     ),
     .target(
       name: "BondryApple",
@@ -60,6 +66,11 @@ let package = Package(
       name: "BondryLocalServer",
       dependencies: ["Bondry", "CBondryLocalServer"],
       path: "apple/Sources/BondryLocalServer"
+    ),
+    .target(
+      name: "BondryRESTServer",
+      dependencies: ["Bondry", "CBondryRESTServer"],
+      path: "apple/Sources/BondryRESTServer"
     ),
     .target(
       name: "BondryEgress",
